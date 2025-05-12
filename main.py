07001827a9458e49fa3c65075ccdc13cdd29ebb1
mohsinahm <35180333+mohsinahm@users.noreply.github.com>
2025-05-12 12:03:04 -0700

    2nd commit done

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from flet import *
 import flet_fastapi
 
 async def main(page: Page):
-    await page.add_async(Text("Hi This is web test for Flet APP", size=30, weight="bold"))
+    await page.add_async(Text("Testing is working bro", size=30, weight="bold"))
 
 app = flet_fastapi.app(main)
